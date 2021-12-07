6f674144c08ad95e3b894b4debcc0d3bdf3a984d
Nguyen Duy <caubebuon03@gmail.comm>
2021-12-07 21:39:54 +0700

    commnet

---
+++ src/app/shared/component/article-detail/comment-item/comment-item.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
+import { CommentsService } from 'src/app/service/comments.service';
+import { Comment } from 'src/app/shared/model/comment.model';
+
+@Component({
+  selector: 'app-comment-item',
+  templateUrl: './comment-item.component.html',
+  styleUrls: ['./comment-item.component.css']
+})
+export class CommentItemComponent implements OnInit {
+  @Input() comment!: Comment;
+
+  constructor(private auth: AuthService, private commentService: CommentsService) { }
+
+  ngOnInit(): void {
+  }
+
+  get isLoggedIn() {
+    return this.auth.isLoggedIn();
+  }
+
+  get currentUser() {
+    return this.auth.getUserName();
+  }
+
+  deleteComment() {
+    this.commentService.deleteEvent.emit(this.comment.id);
+  }
+
+}
