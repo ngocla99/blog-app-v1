c3ce58f0439411a359bbabcee3855fe9d77437f6
ADMIN <tranthanhtung110699@gmail.com>
2021-12-15 13:25:17 +0700

    tag list, fix some display, image

--- src/app/shared/component/extension/tag-list/tag-list.component.ts
+++ src/app/shared/component/extension/tag-list/tag-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeArticleService } from '../../../../service/home-article.service';
 
 @Component({
   selector: 'app-tag-list',
   templateUrl: './tag-list.component.html',
-  styleUrls: ['./tag-list.component.css']
+  styleUrls: ['./tag-list.component.css'],
 })
 export class TagListComponent implements OnInit {
+  tagList!: string[];
 
-  constructor() { }
+  constructor(private homeArticleService: HomeArticleService) {}
 
   ngOnInit(): void {
+    this.homeArticleService.getTagList().subscribe((tagData) => {
+      this.tagList = tagData.tags;
+    });
   }
-
 }
